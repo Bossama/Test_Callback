300ac71d9a29f02d30e133a6f84dcfbc687e2e67
Mohamed-ElHedi <mohamedelhedi.boussama@hotmail.com>
2022-03-16 15:30:54 +0100

    Initial commit

---
+++ test_callback.c
@@ -0,0 +1,26 @@
+/***********************************************
+* A simple C program to demonstrate callback
+************************************************/
+
+#include<stdio.h>
+
+/* Add fonction */ 
+
+/* Sub fonction */ 
+
+int process (char *msg, int p1, int p2, int (* p_Callback_funct)(int, int))
+{
+    //Callback_funct = (int *)p_funct;
+    printf("%s = %d\n",msg, p_Callback_funct(5,3));
+
+}
+
+int main()
+{
+    printf("@add = %d \n", add);
+    process("sum",5, 3, add);
+    process("sub",5, 3, sub);
+    
+    return 0;
+
+}
