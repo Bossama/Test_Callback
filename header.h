f26a600c391e6b7717172645214d5870a593c509
Marwen Boujedaa <marwen.boujedda@actia-engineering.tn>
2022-03-24 08:42:47 +0100

    Create main header file

---
+++ header.h
@@ -0,0 +1,14 @@
+#ifndef CALLBACK_H
+#define CALLBACK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Decalaration variable and function */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CALLBACK_H */
